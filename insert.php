8a55949e732dec4ef0ba4d18a4e5d06eb930a47b
Zhiyuan Xu <k.zhiyuan.xu@gmail.com>
2014-02-16 01:21:51 -0800

    added pop up for pins

---
+++ insert.php
@@ -0,0 +1,26 @@
+<?php
+
+// following tutorial from:
+// http://www.tutorialspoint.com/php/mysql_insert_php.htm
+
+$dbhost = 'localhost:3036';
+$dbuser = 'root';
+$dbpass = 'nincompoop';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+$sql = 'INSERT INTO employee '.
+       '(emp_name,emp_address, emp_salary, join_date) '.
+       'VALUES ( "guest", "XYZ", 2000, NOW() )';
+
+mysql_select_db('test_db');
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not enter data: ' . mysql_error());
+}
+echo "Entered data successfully\n";
+mysql_close($conn);
+?>
