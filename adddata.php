d58f101ef54ba2c5c7ba484a56d82de7b35dcdae
Zhiyuan Xu <k.zhiyuan.xu@gmail.com>
2014-02-17 03:24:21 -0800

    changed input method for entries

---
+++ adddata.php
@@ -0,0 +1,28 @@
+<?php
+
+$db = mysql_connect('localhost', 'root', 'nincompoop');
+
+if (! $db )
+{
+	die('Could not connect: ' . mysql_error());
+}
+
+$_POST['name']
+$_POST['timeplant']
+$_POST['timefound']
+$_POST['lat']
+$_POST['lon']
+
+$sql = 'INSERT INTO marker (id, name, address, lat, lng, type, TimePlanted, TimeFound)' .
+
+$_POST['name']
+$_POST['timeplant']
+$_POST['timefound']
+$_POST['lat']
+$_POST['lon']
+$var = NULL;
+
+
+echo 'Hello';
+
+?>
